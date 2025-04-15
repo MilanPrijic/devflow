82a0ae79183aa9e15fa2e8cd77a9ccbe361ec0fa
Milan Prijic <milan.prijic.92@gmail.com>
2025-04-15 14:54:23 +0200

    Added Dark/Light mode, shadcn, navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import localFont from "next/font/local";
+import ThemeProvider from '@/context/Theme'
+import NavBar from "@/components/navigation/navbar";
 
 const inter = localFont({
     src: "./fonts/InterVF.ttf",
@@ -29,11 +31,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider attribute="class"
+                       defaultTheme="system"
+                       enableSystem
+                       disableTransitionOnChange>
+            <NavBar />
+            {children}
+        </ThemeProvider>
       </body>
     </html>
   );
